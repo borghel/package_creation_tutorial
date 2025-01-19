f500a02343b40b6bf31dad32d501dc79694bf2c4
borghel <borghelborghel3.@gmail.com>
2025-01-19 14:09:53 +0100

    Correction et ajout du code complet pour __init__.py

--- src/package_creation_tutorial/__init__.py
+++ src/package_creation_tutorial/__init__.py
@@ -1,17 +1,7 @@
 
 
 
-# __init__.py
 
-# Importation des fonctions depuis string_ops
-from .string_ops import reverse_string, count_vowels, capitalize_words
-
-# Fonction d'exemple pour les tests ou d'autres usages
 def hello_world():
     return "Bonjour depuis le package_creation_tutorial !"
 
-# Utilisation des fonctions importées
-def example_usage():
-    print(reverse_string("Hello"))  # Utilisation de reverse_string
-    print(count_vowels("Hello"))    # Utilisation de count_vowels
-    print(capitalize_words("hello world"))  # Utilisation de capitalize_words
