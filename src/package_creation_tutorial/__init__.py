2b7fac3a8f7c42d199cc84eb56bf3624f2f33cf2
borghel <borghelborghel3.@gmail.com>
2025-01-19 14:12:58 +0100

    Correction et ajout du code complet pour __init__.py

--- src/package_creation_tutorial/__init__.py
+++ src/package_creation_tutorial/__init__.py
@@ -2,6 +2,10 @@
 
 
 
+# __init__.py
+from .string_ops import reverse_string as reverse_string, count_vowels as count_vowels, capitalize_words as capitalize_words
+
 def hello_world():
     return "Bonjour depuis le package_creation_tutorial !"
+
 
